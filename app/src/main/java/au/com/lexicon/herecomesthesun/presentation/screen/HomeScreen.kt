10722bf510098fc986939ef76784ab8322daf19d
adriantan <adrian.tan@lexicondigital.com.au>
2021-12-21 10:42:59 +1100

    fixed height

--- app/src/main/java/au/com/lexicon/herecomesthesun/presentation/screen/HomeScreen.kt
+++ app/src/main/java/au/com/lexicon/herecomesthesun/presentation/screen/HomeScreen.kt
@@ -1,8 +1,6 @@
 package au.com.lexicon.herecomesthesun.presentation.screen
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
@@ -13,6 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import au.com.lexicon.herecomesthesun.R
 import au.com.lexicon.herecomesthesun.presentation.viewmodel.HomeViewModelContract
 
@@ -44,30 +43,40 @@ fun TopBar(
     navigateToSettings: () -> Unit
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(50.dp)
+            .border(BorderStroke(1.dp, Color.Green))
     ) {
         Column (
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
         }
         Column (
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             //insert logo
         }
         Column (
-            modifier = Modifier.weight(1f),
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
             verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            horizontalAlignment = Alignment.End
         ) {
             //insert settings
             Image(
-                modifier = Modifier.clickable {
+                modifier = Modifier
+                    .clickable {
                     navigateToSettings()
                 },
                 painter = painterResource(id = R.drawable.outline_settings_black),
